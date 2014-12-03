24827b7e8f08ca73582a7bc4093b3e28d280605b
Kiddi <kristinnv12@ru.is>
2014-12-03 12:23:47 +0000

    added name to docker

--- runjob.sh
+++ runjob.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+uname -a
 echo killing docker
-docker kill $(docker ps -q)
+docker kill tictactoe
 
 echo removing docker
-docker rm $(docker ps -a -q)
+docker rm tictactoe
 
 echo pulling docker
 docker pull kristinnv12/tictactoe
 
 echo running docker
-docker run -p 80:8080 -d -e "NODE_ENV=production" kristinnv12/tictactoe
+docker run -p 80:8080 -d -e "NODE_ENV=production" --name="tictactoe" kristinnv12/tictactoe
