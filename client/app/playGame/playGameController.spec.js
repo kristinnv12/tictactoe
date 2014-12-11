bd6190404297ca552fc8dd0ffdeb3e5ec8738891
Kiddi <kristinnv12@ru.is>
2014-12-11 13:33:15 +0000

    playgametesting

---
+++ client/app/playGame/playGameController.spec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+describe('testing playGameController',function(){
+	
+	beforeEach(module('tictactoeApp'));
+	
+	var PlayController, scope, location, httpBackend, http;
+
+	beforeEach(inject(function ($injector, $controller, $rootScope, $http, $location, $interval) {
+		scope = $rootScope.$new();
+		http = $http;
+		httpBackend = $injector.get('$httpBackend');
+		location = $location;
+		location.search('gId', '1337');
+		location.search('playerRole', 'X');
+		PlayController = $controller('PlayGameController', {
+			$scope: scope
+		});
+	}));
+	
+	afterEach(function () {
+		httpBackend.verifyNoOutstandingExpectation();
+		httpBackend.verifyNoOutstandingRequest();
+	});
+
+	it('should give proper values to scope variables: gameName, myself & joinLink', function(){
+
+		init();
+
+		expect(scope.myself).toBe('Kiddi');
+		expect(scope.joinLink).toBe('http://server:80/join/1337');
+		expect(scope.gameState.gameName).toBe('TheBestGame');
+	});
+
+	function init(){
+		 httpBackend.expectGET('/api/history/1337').respond([{
+			event: "GameCreated",
+			name: "TheBestGame",
+			id: "1337",
+			user: {
+				userName: "Kiddi"
+			}
+			}, 
+			{
+			event: "GameJoined",
+			name: "TheBestGame",
+			id: "1337",
+			user: {
+				userName: "Ragnar"
+			}
+			}
+		]);
+		httpBackend.flush();
+	}
+});
