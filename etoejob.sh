c4953f31c97bddf1da1e1cad8054e40466ad8b30
Kiddi <kristinnv12@ru.is>
2014-12-16 19:54:19 +0000

    e2e fix

--- etoejob.sh
+++ etoejob.sh
@@ -2,5 +2,19 @@
 
 set -e
 
+echo Cleaning...
+rm -rf ./dist
+
+echo Building app
+
+echo bower install
+bower install
+
+echo Npm install
+npm install
+
+echo grunt install
+npm install grunt
+
 echo e2e
 grunt test:e2e --port=7000
